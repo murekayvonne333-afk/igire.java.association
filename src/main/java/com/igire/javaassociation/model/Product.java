c2a40947226019a5f55d72fee5a1f0c12f4bbd07
unknown <your-murekayvonne333@gmail.com>
2026-06-17 09:00:12 +0200

    Added Product class

---
+++ src/main/java/com/igire/javaassociation/model/Product.java
@@ -0,0 +1,33 @@
+package com.igire.javaassociation.model;
+
+public class Product {
+
+    private String id;
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(String id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | " + category + " | " + price;
+    }
+}
